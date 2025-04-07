b62227ca06487bd96f314da5ad20081743e27383
Mareeam89 <mareeamraj@gmail.com>
2025-04-07 10:45:03 +0100

    backend

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express")
+const app = express()
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+app.get("/", (req, res) => {
+	res.render("homepage")
+})
 
-// Import Routes
-const sermonRoutes = require('./routes/sermons');
-const devotionalRoutes = require('./routes/devotionals');
-const adminRoutes = require('./routes/admin');
-
-app.use('/api/sermons', sermonRoutes);
-app.use('/api/devotionals', devotionalRoutes);
-app.use('/api/admin', adminRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
